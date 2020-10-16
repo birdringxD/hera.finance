0ef20396f483e5542a148fef622fae5af98a6df5
birdringxD <bird_ring@outlook.com>
2020-10-16 08:49:39 +0800

    fixed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,8 +3,8 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0xf1db7eeaafe189fa6ec04d7d8bc5c9c6e7cb6280'
-
+export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+//0xf1db7eeaafe189fa6ec04d7d8bc5c9c6e7cb6280
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 // a list of tokens by chain
